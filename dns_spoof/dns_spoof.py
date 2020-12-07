a070242653f2fb41bd82bdad90cf10370e69f6a3
aditya-kharb <kharbaditya123@gmail.com>
2020-12-07 16:37:12 +0530

    initial commit

--- dns_spoof/dns_spoof.py
+++ dns_spoof/dns_spoof.py
@@ -1,15 +1,18 @@
 import scapy.all as scapy
+from scapy.layers.dns import DNS
 import netfilterqueue 
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload()) 
-    if scapy_packet.haslayer(scapy.DNSRR):
-        #qname = scapy_packet[scapy.DNSQR].qname
-        print(scapy_packet.show())
-        # print(qname)
-
+    if scapy_packet.haslayer(DNS):
+        qname = scapy_packet[scapy.DNSQR].qname
+        qname_d = qname.decode("utf")
+       # if qname == 'www.bing.com':
+        print(qname_d)
+    # # if scapy_packet.haslayer(scapy.IP):
+    # #     ip_name = scapy_packet[scapy.IP].dst 
+        #print(scapy_packet.show())
     packet.accept()
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(0 , process_packet)
 queue.run()
-process_packet()
